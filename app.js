02f87b11d8629694bc6c1633aad5cffd8aa30998
developersushan <sushansing709sushansing@gmail.com>
2024-01-07 21:19:15 +0600

    added

--- app.js
+++ app.js
@@ -20,15 +20,22 @@ valueDisplays.forEach((valueDisplay)=>{
 function handleFrom(){
 let inputs = document.getElementById('input').value;
 let displayText = document.getElementById('show-text')
+let child = document.getElementById('child')
+let child1 = document.getElementById('child1')
 
  if(inputs<=18){
     displayText.innerHTML=" Sorry You Are Child"
     displayText.style.color="red"
+    child.classList.add('child1')
+    child1.classList.remove('child3')
  }else{
     displayText.innerHTML=" Congratulations You Are Adult"
     displayText.style.color="green"
+    child.classList.remove('child1')
+    child1.classList.add('child3')
+    
  }
  let inputsa = document.getElementById('input').value=""
-
+ child.classList.add('child')
 }
 
